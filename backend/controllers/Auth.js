9d9df500e40b80d601cbf9149f9fb5bbc9a30a13
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-07-19 21:52:46 +0700

    update

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,32 +1,61 @@
 import User from "../models/UserModel.js";
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 
 // Login a user
 export const Login = async (req, res) => {
-  const user = await User.findOne({
-    where: {
-      email: req.body.email,
-    },
-  });
+  try {
+    const { email, password } = req.body; // mendatpakan data email dan password dari pengguna
 
-  if (!user) {
-    return res.status(404).json({ msg: "User not found!" });
+    // Validasi input
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Email and password are required" });
+    }
+
+    // mencari email pengguna
+    const user = await User.findOne({ where: { email } });
+
+    // validasi, jika email pengguna tidak ditemukan
+    if (!user) {
+      return res.status(404).json({ msg: "User not found!" });
+    }
+
+    // verifikasi password pengguna dengan password di database, jika cocok hash lagi
+    const match = await argon2.verify(user.password, password);
+
+    // validasi jika password tidak cocok
+    if (!match) {
+      return res.status(400).json({ msg: "Wrong password!" });
+    }
+
+    // membuat token jwt payload berisi userId dan role pengguna
+    const accessToken = jwt.sign(
+      { userId: user.uuid, role: user.role },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "6h" } // token kadaluarsa dalam 6 jam
+    );
+
+    // menyimpan userId dalam session, untuk menjaga sesi pengguna
+    req.session.userId = user.uuid;
+
+    // mengatus cookie token, yang hanya dapat diakses oleh server httpOnly
+    res.cookie("token", accessToken, {
+      httpOnly: true, // Mencegah akses JavaScript di klien
+      secure: process.env.NODE_ENV === "production", // Hanya mengirim cookie melalui HTTPS di production
+      sameSite: "strict", // Mencegah serangan Cross-Site Request Forgery (CSRF).
+    });
+
+    // mengirim object data pengguna ke user
+    const { uuid, name, role } = user;
+
+    // menampilkan data yang ditampilkan
+    res.status(200).json({ uuid, name, email: user.email, role, accessToken });
+
+    // jika proses validasi gagal, maka tampilkan status 500 dan message
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Internal Server Error" });
   }
-
-  const macth = await argon2.verify(user.password, req.body.password);
-
-  if (!macth) {
-    return res.status(400).json({ msg: "Wrong password!" });
-  }
-
-  req.session.userId = user.uuid;
-
-  const uuid = user.uuid;
-  const name = user.name;
-  const email = user.email;
-  const role = user.role;
-
-  res.status(200).json({ uuid, name, email, role });
 };
 
 // Get me
--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -2,46 +2,73 @@ import User from "../models/UserModel.js";
 import argon2 from "argon2";
 import jwt from "jsonwebtoken";
 
+// Get a user
+export const getUser = async (req, res) => {
+  try {
+    const response = await User.findAll({
+      attributes: ["uuid", "name", "email", "role"],
+    });
+    response.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+// Get user by id
+export const getUserById = async () => {};
+
 // create a user
 export const createUser = async (req, res) => {
-  const { name, email, password, role } = req.body;
+  const { name, email, password, role } = req.body; // mendapatkan data
 
+  // membuat errors yang ditampung didalam array, dan melakukan validasi data
   const errors = [];
   if (!name) errors.push("Name is required");
   if (!email) errors.push("Email is required");
   if (!password) errors.push("Password is required");
+  if (!role) errors.push("Role is required");
 
+  // jika salah satu data tidak dimasukkan, tampilkan status 400 dan message errors
   if (errors.length > 0) {
     return res.status(400).json({ message: errors.join(", ") });
   }
 
+  // membuat password menjadi acaka
   const hasPassword = await argon2.hash(password);
 
   try {
+    // mencari data pengguna dengan email yang ada
     const isUser = await User.findOne({ where: { email } });
 
+    // validasi, jika data email sudah digunakan, tampilkan status 400 dan message
     if (isUser) {
       return res.status(400).json({
         message: "User already exists",
       });
     }
 
+    // jika proses validasi berhasil, ambil semua data pengguna yang melakukan register
     const user = new User({ name, email, password: hasPassword, role });
 
-    // menyimpan data user ke database
+    // menyimpan data pengguna ke database mysql
     await user.save();
 
+    // membuat token jwt
     const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
       expiresIn: "36000m", // token akan expire dalam 6 jam
     });
 
+    // jika berhasil tampilkan data pengguna, token jwt dan message, dengan status 200
     return res.status(200).json({
       user,
       accessToken,
       message: "Register Successfully",
     });
+
+    // jika proses validasi gagal
   } catch (error) {
     console.log(error);
+    // tampilkan status 500(server gagal) dan message
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -51,9 +78,3 @@ export const updateUser = async () => {};
 
 // delete a user
 export const deleteUser = async () => {};
-
-// Get a user
-export const getUser = async () => {};
-
-// Get user by id
-export const getUserById = async () => {};
