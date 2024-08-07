50398775291de4590699679baeeb427a701bb1a7
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-08-07 16:07:31 +0700

    update

--- frontend/combine/src/pages/SignUp/ForgotPassword.jsx
+++ frontend/combine/src/pages/SignUp/ForgotPassword.jsx
@@ -1,57 +1,73 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Toast from "../../components/ToastMessage/Toast";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const kirim = async (e) => {
+    e.preventDefault();
+    if (!email) {
+      toast.error("Email wajib diisi");
+    } else {
+      try {
+        await axios.put("http://localhost:8080/forgot-password", { email });
+        setEmail("");
+        toast.success("Silahkan cek email anda");
+      } catch (error) {
+        toast.error("Terjadi kesalahan saat mereset password");
+        console.error("There was an error resetting the password!", error);
+      }
+    }
+  };
+
   return (
     <section className="bg-gray-500 min-h-screen flex items-center justify-center">
+      <Toast />
       <div className="w-full max-w-md mx-auto p-4">
         <form
-          //   onSubmit={Auth}
+          onSubmit={kirim}
           className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
         >
-          {/* {isError && <p className="text-center text-red-500">{message}</p>} */}
-          {/* {error && <p className="text-center text-red-500">{error}</p>} */}
           <h1 className="text-2xl font-bold text-center mb-6">
-            Change Password
+            Reset Password
           </h1>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
+              htmlFor="email"
             >
-              Password
+              Email
             </label>
             <input
               type="text"
-              id="password"
+              id="email"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="**********"
-              //   value={email}
-              //   onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              value={email}
+              onChange={changeEmail}
             />
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Confpassword"
-            >
-              Confirm Password
-            </label>
-            <input
-              type="Confpassword"
-              id="Confpassword"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="**********"
-              //   value={password}
-              //   onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col justify-between">
             <button
               type="submit"
               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
             >
               Reset Password
             </button>
+            <p className="text-sm text-center mt-4">
+              Do you have an account?{" "}
+              <Link to="/" className="text-blue-500 hover:underline">
+                Login
+              </Link>
+            </p>
           </div>
         </form>
       </div>
